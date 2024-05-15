d94dd2b6ad1aa5663ff3176100dd6fccfe4c7aa0
3dida <44850191+aallali@users.noreply.github.com>
2024-05-15 16:38:13 +0100

    [+] : mongoose setup : @ Server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import connectDB from './config/db';
 
 const app = express();
 
@@ -8,6 +8,8 @@ dotenv.config(); // 3afak khdmi
 
 const PORT = process.env.PORT || 4242;
 
+// console.log(process.env.MONGO_URL, process.env.PORT)
+connectDB();
 app.get('/health', (req, res) => {
     res.json({
         status: 'OK',
