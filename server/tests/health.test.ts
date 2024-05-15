83e505af0aee45684ae0299a809cc9b83992471c
3dida <44850191+aallali@users.noreply.github.com>
2024-05-15 19:59:29 +0100

    [-] : remove console.log

--- server/tests/health.test.ts
+++ server/tests/health.test.ts
@@ -2,10 +2,10 @@
 import request from 'supertest';
 import app from '../src/index';
 
+
 describe('GET /health', () => {
     it('should return status OK', async () => {
         const response = await request(app).get('/health');
-        console.log(response.body)
         expect(response.status).toBe(200);
         expect(response.body.status).toBe('OK');
         expect(response.body.message).toBe('Nadi Kanadi');
