9d0492eaa61f314879f20d8a60232a34c047609c
3dida <44850191+aallali@users.noreply.github.com>
2024-05-15 14:45:45 +0100

    [feat] : skeleton UI

---
+++ ui/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <p className='text-7xl'>Salam</p>
+      <p className='text-lg'>Hello World</p>
+      <p className='font-semibold'>Semi bold Bold</p>
+      <p className='font-bold'>Bold</p>
+      <p className='font-extrabold'>Extra Bold</p>
+    </div>
+  );
+}
+
+export default App;
