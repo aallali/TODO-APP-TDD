1ad0492fdc558b02b7128789d623d50b3fdb2212
3dida <44850191+aallali@users.noreply.github.com>
2024-05-17 01:45:47 +0100

    [+] : implement modules in App.tsx : @ UI

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import AuthForm from './modules/auth/Index';
+import { useAuthStore } from './modules/auth/auth.store';
+import Tasks from './modules/tasks';
+
 
 function App() {
+  const { userId } = useAuthStore();
   return (
     <div className="">
-      <p className='text-7xl'>Salam</p>
-      <p className='text-lg'>Hello World</p>
-      <p className='font-semibold'>Semi bold Bold</p>
-      <p className='font-bold'>Bold</p>
-      <p className='font-extrabold'>Extra Bold</p>
+      <div className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style={{
+        backgroundImage: 'url("https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80")'
+      }}>
+        {
+          userId ? <Tasks /> : <AuthForm />
+        }
+      </div>
     </div>
   );
 }
